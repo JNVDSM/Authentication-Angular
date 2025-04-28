e8d0cfcedf53e1e0834ad694f8c3b4c03f967b94
Jhansi_Mannidi <jhansimannidi@gmail.com>
2025-04-28 22:14:29 +0530

    Routes is modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signUp/signUp.component';
 import { SuccessComponent } from './components/success/success.component';
+import { ForgotComponent } from './components/forgot/forgot.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signUp', component: SignupComponent },
   { path: 'loginSuccess', component: SuccessComponent },
+  {path:'forgot',component:ForgotComponent},
   { path: '**', redirectTo: '/login' }
 ];
